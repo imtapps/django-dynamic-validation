e3f4d634cbfba241f540011608f52ac3b9440cb6
Aaron Madison <aaron.madison@theimtgroup.com>
2011-07-19 13:01:18 -0500

    added two additional sample rules

--- example/sample/dynamic_actions.py
+++ example/sample/dynamic_actions.py
@@ -11,3 +11,24 @@ class GenderRatio(object):
     fields = {
         'percent': forms.IntegerField(help_text="Ratio of Men to Women"),
     }
+
+@site.register
+class LimitNumberOfPlayersUnderAge(object):
+    key = "LimitNumberOfPlayersUnderAge"
+    display_name = "Limit the number of players under a given age"
+
+    fields = {
+        'players': forms.IntegerField(help_text="Maximum players allowed below age."),
+        'age': forms.IntegerField(help_text="Minimum age."),
+    }
+
+@site.register
+class RangeOfAveragePlayerAge(object):
+    key = "RangeOfAveragePlayerAge"
+    display_name = "the average age of all players on a team must be within an allowed range."
+
+    fields = {
+        'players': forms.IntegerField(help_text="Maximum players allowed below age."),
+        'min_age': forms.IntegerField(help_text="Minimum average age allowed."),
+        'max_age': forms.IntegerField(help_text="Maximum average age allowed."),
+    }
