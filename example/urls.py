6bdeb302dcfbe390fce864ac8d6250a3828e5108
Aaron Madison <aaron.madison@theimtgroup.com>
2011-07-20 09:34:36 -0500

    added sample view to display rule violations

--- example/urls.py
+++ example/urls.py
@@ -1,6 +1,10 @@
 from django.conf.urls.defaults import patterns, include, url
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 urlpatterns = patterns('',
+    url(r'^$', TemplateView.as_view(
+        template_name="sample/index.html",
+    ),),
     url(r'^admin/', include(admin.site.urls)),
 )
