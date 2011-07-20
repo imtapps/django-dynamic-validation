37f4121903c02d9ce1418c24fe5c913edc589224
Aaron Madison <aaron.madison@theimtgroup.com>
2011-07-20 16:24:43 -0500

    added Violation model to manager

--- dynamic_validation/admin.py
+++ dynamic_validation/admin.py
@@ -7,4 +7,8 @@ class RuleAdmin(BaseAjaxModelAdmin):
     form = admin_forms.RuleForm
     list_display = ('name', 'group_object')
 
+class ViolationAdmin(admin.ModelAdmin):
+    list_display = ('rule', 'key', 'validation_object', 'message', 'violated_fields')
+    list_filter = ('rule', )
 admin.site.register(models.Rule, RuleAdmin)
+admin.site.register(models.Violation, ViolationAdmin)
