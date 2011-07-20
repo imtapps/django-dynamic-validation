1fd86a3fe08147b7f84f17a0b5d91a8c27b925dc
Aaron Madison <aaron.madison@theimtgroup.com>
2011-07-20 08:53:03 -0500

    introduced a bad apple

--- dynamic_validation/tests/__init__.py
+++ dynamic_validation/tests/__init__.py
@@ -1,3 +1,8 @@
 from dynamic_validation.tests.test_models import *
 from dynamic_validation.tests.test_registry import *
 from dynamic_validation.tests.test_admin_forms import *
+
+from django.utils import unittest
+class BadTest(unittest.TestCase):
+    def test_is_bad(self):
+        self.fail("bad apple")
